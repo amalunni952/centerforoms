c37d2f671c98fa917bef057859d0f3b09ad4706d
amalunni952 <85153573+amalunni952@users.noreply.github.com>
2026-03-17 10:25:13 +0530

    Update image_js.js
    js

--- image_js.js
+++ image_js.js
@@ -1,27 +1,50 @@
- 
-
 (function () {
-  var oldUrl = "https://gp-assets-1.growthplug.com/website_files/5120/Dentist_Huntersville_NC_logo.png";
   var newUrl = "https://www.centerforoms.com/wp-content/uploads/2026/03/Dentist_Huntersville_NC_logo-symbol.webp";
 
   function replaceHeaderLogo() {
-    document.querySelectorAll('img[src="' + oldUrl + '"]').forEach(function (img) {
-      img.src = newUrl;
+    var selectors = [
+      'img[alt*="Brand Logo"]',
+      'img[title*="Brand Logo"]',
+      'a.navbar-brand img',
+      'a.logo img',
+      'header img'
+    ];
+
+    selectors.forEach(function (selector) {
+      document.querySelectorAll(selector).forEach(function (img) {
+        img.src = newUrl;
+        img.setAttribute("src", newUrl);
+        if (img.srcset) {
+          img.srcset = newUrl;
+          img.setAttribute("srcset", newUrl);
+        }
+      });
     });
   }
 
-  replaceHeaderLogo();
-
-  var observer = new MutationObserver(function () {
+  function init() {
     replaceHeaderLogo();
-  });
 
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-    attributeFilter: ["src"]
-  });
+    var observer = new MutationObserver(function () {
+      replaceHeaderLogo();
+    });
+
+    if (document.body) {
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ["src", "srcset"]
+      });
+    }
+
+    setInterval(replaceHeaderLogo, 1500);
+    window.addEventListener("load", replaceHeaderLogo);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
+  }
 })();
-
- 
